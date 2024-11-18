e047be7108d0023c910ce926a49b403fd7eb31de
davi18904 <davi8904@student.acitech.org>
2024-11-18 10:47:35 -0500

    11.18.2024
    I added some decode and encode values as well as got a basic understanding of the requirements. I don't have anything fully working yet.

--- enigma_template.py
+++ enigma_template.py
@@ -1,30 +1,10 @@
 # enigma.py
 # description: a simple rotational ciphertext program that can create
 # custom encoded messages, as well as encode and decode from file.
-# author: YOUR_NAME_HERE
-# created: MM.DD.YYYY
-# last update:  MM.DD.YYYY
+# author: cdavis
+# created: 11.18.24
+# last update:  11.18.24
 import random
-
-# we'll be using this string for the majority of our translations
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-# user inputs a message and selects a key (or random), the message is then translated using the cipher
-def encode_message():
-    pass
-
-# encodes a target file, similarly to encode_message, except now targeting a filename
-def encode_file():
-    pass
-
-# decodes target file using a user-specified key. If key is unknown, a keypress should
-# call decode_unknown_key()
-def decode_file():
-    pass
-
-# runs if the key is unknown. If this is true, print out all possible decoding combinations.
-def decode_unknown_key(filename):
-   pass
 
 
 # main method declaration
@@ -40,7 +20,7 @@ def main():
         selection = input("Choose an option:")
 
         if selection == "1":
-            encode_message()
+            .file()
         elif selection == "2":
             encode_file()
         elif selection == "3":
@@ -50,6 +30,30 @@ def main():
             exit()
         else:
             print("Invalid choice. Please try again.")
+            # we'll be using this string for the majority of our translations
+            alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+            # user inputs a message and selects a key (or random), the message is then translated using the cipher
+            string = ""
+
+            encoded_string = string.encode('utf-8')
+            print('The encoded string in base64 format is :')
+            print(encoded_string)
+
+            # encodes a target file, similarly to encode_message, except now targeting a filename
+            def encode_file():
+                pass
+
+            # decodes target file using a user-specified key. If key is unknown, a keypress should
+            # call decode_unknown_key()
+            decoded_string = string.decode('utf-8')
+            print('The decoded string is :')
+            print(decoded_string)
+
+            # runs if the key is unknown. If this is true, print out all possible decoding combinations.
+            def decode_unknown_key(filename):
+                pass
+
 
 # runs on program start
 if __name__ == "__main__":

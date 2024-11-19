6340153c60b4a4db452599124469bf4493f004d3
davi18904 <davi8904@student.acitech.org>
2024-11-19 10:49:26 -0500

    11.19.24
    I got a better understanding and I added some stuff to the values that allow the user to make a selection.

--- enigma_template.py
+++ enigma_template.py
@@ -3,11 +3,11 @@
 # custom encoded messages, as well as encode and decode from file.
 # author: cdavis
 # created: 11.18.24
-# last update:  11.18.24
+# last update:  11.19.24
 import random
 
 
-# main method declaration
+# main menu allowing user to select how they would like to interpret the file
 def main():
     while True:
         print(f"Welcome to the Enigma Machine!\n"
@@ -17,16 +17,19 @@ def main():
               f"[3]: Decode file.\n"
               f"[4]: Exit.")
 
+# based on user input the task will be executed by reading the file in a specific way
         selection = input("Choose an option:")
 
+        # user inputs a message and selects a key (or random), the message is then translated using the cipher
         if selection == "1":
-            .file()
+
+    # encodes a target file, similarly to encode_message, except now targeting a filename
         elif selection == "2":
-            encode_file()
+
         elif selection == "3":
-            decode_file()
+
         elif selection == "4":
-            print("Goodbye.")
+            print("Adios Amigo!.")
             exit()
         else:
             print("Invalid choice. Please try again.")
@@ -34,21 +37,21 @@ def main():
             alphabet = "abcdefghijklmnopqrstuvwxyz"
 
             # user inputs a message and selects a key (or random), the message is then translated using the cipher
-            string = ""
-
-            encoded_string = string.encode('utf-8')
-            print('The encoded string in base64 format is :')
-            print(encoded_string)
 
             # encodes a target file, similarly to encode_message, except now targeting a filename
+            # letter at index(((location of letter) + key) % 26)
+
+            def encode_message():
+                message = input("What's your message?")
+                key = int(input("What's your key?"))
+                pass
+
             def encode_file():
+
                 pass
 
             # decodes target file using a user-specified key. If key is unknown, a keypress should
             # call decode_unknown_key()
-            decoded_string = string.decode('utf-8')
-            print('The decoded string is :')
-            print(decoded_string)
 
             # runs if the key is unknown. If this is true, print out all possible decoding combinations.
             def decode_unknown_key(filename):
